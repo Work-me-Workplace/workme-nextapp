76b14435311291a0ae1db8f6dcf065f075898bec
Adam Cole <adam@gofastcrushgoals.com>
2025-11-19 21:26:16 -0500

    Add Firebase config file with frontend config values

---
+++ lib/firebase.config.ts
@@ -0,0 +1,16 @@
+/**
+ * Firebase Configuration
+ * 
+ * Frontend config values for Firebase client SDK
+ */
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCzaBE06YwU9c6zUzB1jnlTU3NHtBuAmaA",
+  authDomain: "workme-b49ef.firebaseapp.com",
+  projectId: "workme-b49ef",
+  storageBucket: "workme-b49ef.firebasestorage.app",
+  messagingSenderId: "566305819856",
+  appId: "1:566305819856:web:201c19e5d813ed6c1762d5",
+  measurementId: "G-JFDDEX6NJC"
+}
+
--- lib/firebaseClient.ts
+++ lib/firebaseClient.ts
@@ -8,15 +8,17 @@
 'use client'
 
 import { initializeApp, getApps, FirebaseApp } from 'firebase/app'
+import { firebaseConfig as defaultConfig } from './firebase.config'
 
+// Use env vars if set, otherwise use config file values
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || '',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || defaultConfig.apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || defaultConfig.authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || defaultConfig.projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || defaultConfig.storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || defaultConfig.messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || defaultConfig.appId,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || defaultConfig.measurementId,
 }
 
 // Only initialize in browser and if config is valid
