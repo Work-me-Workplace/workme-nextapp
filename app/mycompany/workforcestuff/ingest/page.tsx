7193f229cb0df755988ccf9519d0e0ed71bddc01
Adam Cole <adam@gofastcrushgoals.com>
2025-11-28 16:59:57 -0500

    fix: Remove obsolete checkExistingWorkspace call and clean up ingest UI

--- app/mycompany/workforcestuff/ingest/page.tsx
+++ app/mycompany/workforcestuff/ingest/page.tsx
@@ -23,7 +23,6 @@ export default function WorkforceStuffIngestPage() {
         router.push('/signin')
       } else {
         setWorkMeId(id)
-        checkExistingWorkspace(id)
       }
     }
   }, [router])
