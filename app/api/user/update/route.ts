5dc883729439b50a40215911a1c32d617a16f7c1
Adam Cole <adam@gofastcrushgoals.com>
2025-12-04 16:05:22 -0500

    Remove domain field from user/update route

--- app/api/user/update/route.ts
+++ app/api/user/update/route.ts
@@ -71,10 +71,9 @@ export async function POST(request: NextRequest) {
       },
       include: {
         companyUnit: {
-      select: {
-        id: true,
+          select: {
+            id: true,
             name: true,
-            domain: true,
           },
         },
       },
