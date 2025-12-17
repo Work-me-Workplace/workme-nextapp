3ced96001e56d81ea23501d494b125c5a08f265d
Adam Cole <adam@gofastcrushgoals.com>
2025-12-17 14:37:11 -0500

    Add type declaration for unfluff module

---
+++ types/unfluff.d.ts
@@ -0,0 +1,19 @@
+declare module 'unfluff' {
+  interface UnfluffData {
+    title?: string
+    text?: string
+    description?: string
+    author?: string | string[]
+    publisher?: string
+    image?: string
+    date?: string
+    lang?: string
+    favicon?: string
+    canonicalLink?: string
+    tags?: string[]
+    videos?: any[]
+  }
+
+  function unfluff(html: string, language?: string): UnfluffData
+  export = unfluff
+}
